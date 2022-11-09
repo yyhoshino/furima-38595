4bc06f4e498e05b03e6e5df4f321e8e4f1792d48
yyhoshino <rozary35@gmail.com>
2022-11-09 18:34:13 +0900

    deviseの導入からバリデーションの設定まで

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_kana,presence: true
+  validates :last_name_kana,presence: true
+  validates :birthday,presence: true
+
+end
