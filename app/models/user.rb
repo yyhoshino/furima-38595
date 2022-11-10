25e60ad385b56f66aa94fd2ef84c0e2f16248d29
yyhoshino <rozary35@gmail.com>
2022-11-10 11:24:15 +0900

    新規登録機能、ログイン、ログアウト機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana,presence: true
-  validates :last_name_kana,presence: true
-  validates :birthday,presence: true
+  validates :birthday, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+  
 
 end
