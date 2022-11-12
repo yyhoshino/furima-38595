36d656d6ce6a180a48022dd511ab79bd64701d3b
yyhoshino <rozary35@gmail.com>
2022-11-12 20:35:23 +0900

    商品一覧表示機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   def index
+    @products = Product.all.order(id: "DESC" )
   end
 
   def new
