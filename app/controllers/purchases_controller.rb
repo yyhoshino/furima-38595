92e7dd511198262cf3623a09072f346b6faa5c3a
yyhoshino <rozary35@gmail.com>
2022-11-16 22:21:02 +0900

    Pay.jsを用いた決済処理の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -8,6 +8,7 @@ class PurchasesController < ApplicationController
     @product = Product.find(params[:product_id])
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
+       pay_item
       @purchase_address.save
       redirect_to root_path
      else
@@ -19,11 +20,20 @@ class PurchasesController < ApplicationController
 
   def purchase_params
     params.require(:purchase_address).permit(:post_code, :prefecture_id, :city, :address, :building_name,
-                                             :phone_number).merge(user_id: current_user.id,product_id: params[:product_id])
+                                             :phone_number).merge(user_id: current_user.id,product_id: params[:product_id],token: params[:token])
   end
 
   def product_params
     params.require(:product).permit(:name,:price,:shipping_charge_id,:image)
   end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+       Payjp::Charge.create(
+         amount: @product.price,
+         card: purchase_params[:token],
+         currency: 'jpy'
+      )
+    end
 
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,8 +1,9 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :purchase, :user_id, :product_id
+  attr_accessor :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :purchase, :user_id, :product_id, :token
 
   with_options presence: true do
+    validates :token
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :phone_number, format: {with: /\A\d{10,11}\z/}
