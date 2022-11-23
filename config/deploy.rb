bc7bc0600c811f9b078967d556fada887c61b97c
yyhoshino <rozary35@gmail.com>
2022-11-23 17:26:21 +0900

    deploy.rbの修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,9 @@ set :application, 'furima-38595'
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:yyhoshino/furima-38595.git'
 
+# ブランチを指定する
+set :branch, "main"
+
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
