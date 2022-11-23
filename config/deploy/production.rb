0aec782225c53e22c6c4ecf3fadcd326a89cf5d7
yyhoshino <rozary35@gmail.com>
2022-11-23 15:07:32 +0900

    productiion.rbの修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '54.168.82.144 ', user: 'ec2-user', roles: %w{app db web}
+server '54.168.82.144', user: 'ec2-user', roles: %w{app db web}
